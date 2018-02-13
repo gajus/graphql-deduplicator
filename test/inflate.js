856d254dd402e5710d62d1527db98cb0549147a1
Gajus Kuizinas <gajus@gajus.com>
2018-02-13 18:08:20 +0000

    feat: add inflate method

---
+++ test/inflate.js
@@ -0,0 +1,107 @@
+// @flow
+
+/* eslint-disable id-match */
+
+import test from 'ava';
+import inflate from '../src/inflate';
+
+test('inflates a deflated object', (t) => {
+  const deflatedResponse = {
+    data: [
+      {
+        __typename: 'foo',
+        id: 1,
+        name: 'bar'
+      },
+      {
+        __typename: 'foo',
+        id: 1
+      }
+    ]
+  };
+
+  const inflatedResponse = inflate(deflatedResponse);
+
+  t.deepEqual(inflatedResponse, {
+    data: [
+      {
+        __typename: 'foo',
+        id: 1,
+        name: 'bar'
+      },
+      {
+        __typename: 'foo',
+        id: 1,
+        name: 'bar'
+      }
+    ]
+  });
+});
+
+test('inflates a deflated object (nested; different path)', (t) => {
+  const deflatedResponse = {
+    data: [
+      {
+        __typename: 'foo',
+        bar1: {
+          __typename: 'bar',
+          id: 1,
+          name: 'bar1'
+        },
+        bar2: {
+          __typename: 'bar',
+          id: 1,
+          name: 'bar2'
+        },
+        id: 1
+      },
+      {
+        __typename: 'foo',
+        bar1: {
+          __typename: 'bar',
+          id: 1
+        },
+        bar2: {
+          __typename: 'bar',
+          id: 1
+        },
+        id: 2
+      }
+    ]
+  };
+
+  const inflatedResponse = inflate(deflatedResponse);
+
+  t.deepEqual(inflatedResponse, {
+    data: [
+      {
+        __typename: 'foo',
+        bar1: {
+          __typename: 'bar',
+          id: 1,
+          name: 'bar1'
+        },
+        bar2: {
+          __typename: 'bar',
+          id: 1,
+          name: 'bar2'
+        },
+        id: 1
+      },
+      {
+        __typename: 'foo',
+        bar1: {
+          __typename: 'bar',
+          id: 1,
+          name: 'bar1'
+        },
+        bar2: {
+          __typename: 'bar',
+          id: 1,
+          name: 'bar2'
+        },
+        id: 2
+      }
+    ]
+  });
+});
