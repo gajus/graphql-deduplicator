4120ffc176da7ccd9cec89fd02f73fdd7bf571b3
Rune Carlsen <rc@emendo.com>
2020-07-11 10:42:59 +0200

    Fix flow warnings

--- src/deflate.js
+++ src/deflate.js
@@ -1,55 +1,53 @@
 // @flow
 
+type NodeType = Object | $ReadOnlyArray<NodeType>;
+
 // eslint-disable-next-line complexity
-const deflate = (node: Object, index: Object, path: $ReadOnlyArray<string>) => {
-  if (node && node.id && node.__typename) {
-    const route = path.join(',');
-
-    if (index[route] && index[route][node.__typename] && index[route][node.__typename][node.id]) {
-      return {
-        // eslint-disable-next-line id-match
-        __typename: node.__typename,
-        id: node.id,
-      };
-    } else {
-      if (!index[route]) {
-        index[route] = {};
+const deflate = (node: NodeType, index: Object, path: $ReadOnlyArray<string>): NodeType => {
+  if (Array.isArray(node)) {
+    return node.map((childNode) => {
+      if (typeof childNode === 'string' || typeof childNode === 'number' || typeof childNode === 'boolean') {
+        return childNode;
+      } else {
+        return deflate(childNode, index, path);
       }
+    });
+  } else {
+    if (node && node.id && node.__typename) {
+      const route = path.join(',');
 
-      if (!index[route][node.__typename]) {
-        index[route][node.__typename] = {};
-      }
-
-      index[route][node.__typename][node.id] = true;
-    }
-  }
-
-  const fieldNames = Object.keys(node);
-
-  const result = Array.isArray(node) ? [] : {};
-
-  for (const fieldName of fieldNames) {
-    const value = node[fieldName];
-
-    if (Array.isArray(value)) {
-      // $FlowFixMe
-      result[fieldName] = value.map((childNode) => {
-        if (typeof childNode === 'string' || typeof childNode === 'number' || typeof childNode === 'boolean') {
-          return childNode;
+      if (index[route] && index[route][node.__typename] && index[route][node.__typename][node.id]) {
+        return {
+          // eslint-disable-next-line id-match
+          __typename: node.__typename,
+          id: node.id,
+        };
+      } else {
+        if (!index[route]) {
+          index[route] = {};
         }
 
-        return deflate(childNode, index, path.concat([fieldName]));
-      });
-    } else if (typeof value === 'object' && value !== null) {
-      // $FlowFixMe
-      result[fieldName] = deflate(value, index, path.concat([fieldName]));
-    } else {
-      // $FlowFixMe
-      result[fieldName] = value;
-    }
-  }
+        if (!index[route][node.__typename]) {
+          index[route][node.__typename] = {};
+        }
 
-  return result;
+        index[route][node.__typename][node.id] = true;
+      }
+    }
+    const fieldNames = Object.keys(node);
+    const result = {};
+    for (const fieldName of fieldNames) {
+      const value = node[fieldName];
+
+      if (Array.isArray(value) || typeof value === 'object' && value !== null) {
+        result[fieldName] = deflate(value, index, path.concat([fieldName]));
+      } else {
+        result[fieldName] = value;
+      }
+    }
+
+    return result;
+  }
 };
 
 export default (response: Object) => {
--- src/inflate.js
+++ src/inflate.js
@@ -1,51 +1,49 @@
 // @flow
 
+type NodeType = Object | $ReadOnlyArray<NodeType>;
+
 // eslint-disable-next-line complexity
-const inflate = (node: Object, index: Object, path: $ReadOnlyArray<string>) => {
-  if (node && node.id && node.__typename) {
-    const route = path.join(',');
+const inflate = (node: NodeType, index: Object, path: $ReadOnlyArray<string>): NodeType => {
+  if (Array.isArray(node)) {
+    return node.map((childNode) => {
+      if (typeof childNode === 'string' || typeof childNode === 'number' || typeof childNode === 'boolean') {
+        return childNode;
+      } else {
+        return inflate(childNode, index, path);
+      }
+    });
+  } else {
+    if (node && node.id && node.__typename) {
+      const route = path.join(',');
 
-    if (index[route] && index[route][node.__typename] && index[route][node.__typename][node.id]) {
-      return index[route][node.__typename][node.id];
+      if (index[route] && index[route][node.__typename] && index[route][node.__typename][node.id]) {
+        return index[route][node.__typename][node.id];
+      }
+
+      if (!index[route]) {
+        index[route] = {};
+      }
+
+      if (!index[route][node.__typename]) {
+        index[route][node.__typename] = {};
+      }
+
+      index[route][node.__typename][node.id] = node;
+    }
+    const fieldNames = Object.keys(node);
+    const result = {};
+    for (const fieldName of fieldNames) {
+      const value = node[fieldName];
+
+      if (Array.isArray(value) || typeof value === 'object' && value !== null) {
+        result[fieldName] = inflate(value, index, path.concat([fieldName]));
+      } else {
+        result[fieldName] = value;
+      }
     }
 
-    if (!index[route]) {
-      index[route] = {};
-    }
-
-    if (!index[route][node.__typename]) {
-      index[route][node.__typename] = {};
-    }
-
-    index[route][node.__typename][node.id] = node;
+    return result;
   }
-
-  const fieldNames = Object.keys(node);
-
-  const result = Array.isArray(node) ? [] : {};
-
-  for (const fieldName of fieldNames) {
-    const value = node[fieldName];
-
-    if (Array.isArray(value)) {
-      // $FlowFixMe
-      result[fieldName] = value.map((childNode) => {
-        if (typeof childNode === 'string' || typeof childNode === 'number' || typeof childNode === 'boolean') {
-          return childNode;
-        }
-
-        return inflate(childNode, index, path.concat([fieldName]));
-      });
-    } else if (typeof value === 'object' && value !== null) {
-      // $FlowFixMe
-      result[fieldName] = inflate(value, index, path.concat([fieldName]));
-    } else {
-      // $FlowFixMe
-      result[fieldName] = value;
-    }
-  }
-
-  return result;
 };
 
 export default (response: Object) => {
